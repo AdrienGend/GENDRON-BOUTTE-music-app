085e820f54da2c5932d83092e77a61399e53d122
M1txY <113921255+M1txY@users.noreply.github.com>
2023-05-15 10:06:16 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,6 @@
-document.addEventListener('DOMContentLoaded', function app() {
-    fetch('html/app.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('app_ctn').innerHTML = data;
-        });
 
-    const btn_changepage = document.getElementById('btn_changepage');
-    btn_changepage.addEventListener('click', function () {
-        fetch('html/page2.html')
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById('app_ctn').innerHTML = data;
-            });
+fetch('html/app.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('app_ctn').innerHTML = data;
     });
-});
