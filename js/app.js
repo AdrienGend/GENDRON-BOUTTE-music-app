afe0cbaeaf03262ece2f7243ef38f63218203262
M1txY <113921255+M1txY@users.noreply.github.com>
2023-05-16 17:09:25 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 let homepage = `<div style="color: #fff">
 test app
-</div>
-<button id="btn_changepage">changement de page</button>
-`;
+</div>`;
 
-let searchpage = `<input type="text" id="search_input" placeholder="Rechercher une musique">
-`;
+let searchpage = `<input type="text" id="search_input" placeholder="Rechercher une musique">`;
+
+let biblio = `<div style="color: #fff">
+test biblio
+</div>`;
+
+const base_body_color = "#000";
 
 
 
@@ -18,11 +21,42 @@ document.addEventListener('DOMContentLoaded', function () {
 
     bouton_accueil.addEventListener('click', () => {
         document.getElementById('app_ctn').innerHTML = homepage;
+        document.body.style.background = base_body_color;
+        document.body.style.background = "#098a5a";
     });
 
     bouton_recherche.addEventListener('click', () => {      
         document.getElementById('app_ctn').innerHTML = searchpage;
+        document.body.style.background = base_body_color;
+        document.body.style.background = "#fda707";
     });
+
+    bouton_biblio.addEventListener('click', () => {
+        document.getElementById('app_ctn').innerHTML = biblio;
+        document.body.style.background = base_body_color;
+        document.body.style.background = "#d2d4ff";
+    });
+
+
+    
+    let token = "";
+
+    let client_id = "a1e952d70faf431993ea9dce2b15a001";
+    let client_secret = "0e26f83dd700431bb3cda5d0c2acdafd";
+    
+    SpotifyAuthentification().then(r => console.log("Authentification done"));
+    
+    
+    async function SpotifyAuthentification() {
+        token = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: `grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`
+        }).then(response => response.json()).catch(error => console.log(error));
+    }
+
 
       
   });
