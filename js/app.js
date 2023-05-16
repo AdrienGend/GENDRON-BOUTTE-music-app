948a82536bdce8685f77c0eec99a98fb22e21ea2
M1txY <113921255+M1txY@users.noreply.github.com>
2023-05-16 13:03:36 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 
-fetch('html/app.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('app_ctn').innerHTML = data;
-    });
+const home_btn = document.getSelectionById('home_btn');
+const search_btn = document.setElementById('search_btn');
+const lib_btn = document.setElementById('lib_btn');
+
+home_btn.addEventListener('click', () => {
+    fetch('html/home.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('app_ctn').innerHTML = data;
+        });
+});
